a669607dd1eacd3c5d0363744be2860dbbe977ac
caue <cauepaivalira@outlook.com>
2024-03-14 21:01:18 -0300

    first commit do projeto do joquinho

---
+++ java_folder/projeto_joguinho/Jogo.java
@@ -0,0 +1,27 @@
+package java_folder.projeto_joguinho;
+
+import java_folder.projeto_joguinho.Tabuleiro;
+
+public class Jogo {
+  
+    
+   Jogo(final int tamanho){
+
+   }
+
+   public void jogar(){
+      
+   }
+
+   private void inputUsuario(){
+
+   }
+
+   private void mapearComandos(){
+
+   }
+
+   
+      
+
+}
