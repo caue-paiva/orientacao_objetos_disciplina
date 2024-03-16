2715e3a93423fe598dec6ebf87d81849db5f3483
caue <cauepaivalira@outlook.com>
2024-03-15 23:57:02 -0300

    primeiros codigos em python

---
+++ python_folder/joguinho/jogo.py
@@ -0,0 +1,62 @@
+from tabuleiro import Tabuleiro
+
+
+class Jogo:
+
+   MOVIMENTOS_POSSIVEIS = "UDLR"
+
+   tamanho_tabul:int
+   tabuleiro: Tabuleiro
+
+   def __init__(self, tamanho:int) -> None:
+      self.tamanho_tabul = tamanho
+      self.tabuleiro = Tabuleiro(tamanho)
+
+   def jogar(self)->None:
+      print("Vamos comecar o jogo!!")
+
+      while(not self.tabuleiro.condicao_vitoria()):
+          self.__rodada()
+      
+      print("Parabéns, voce venceu")
+   
+   def __rodada(self)->None:
+      self.tabuleiro.print_tabuleiro()
+      movimento: str = self.__user_input()
+      if not movimento:
+         return
+      self.__realizar_movimento(movimento)
+      
+   def __user_input(self)->str:
+      movimento:str = input("Qual o proximo movimento? U/D/L/R ?: ").upper()
+      if movimento not in self.MOVIMENTOS_POSSIVEIS:
+         print("Movimento invalido")
+         return ""
+      return movimento
+   
+   def __realizar_movimento(self,movimento:str)->None:
+      linha_whitespace:int = self.tabuleiro.posicao_white_space[0]
+      coluna_whitespace:int = self.tabuleiro.posicao_white_space[1]
+      print(f"linha {linha_whitespace} , coluna {coluna_whitespace}")
+
+      match(movimento):
+         case "U":
+            if not self.tabuleiro.troca_posicoes(linha_whitespace,coluna_whitespace,linha_whitespace-1,coluna_whitespace):
+               print("Movimento invalido")
+         
+         case "D":
+            if not self.tabuleiro.troca_posicoes(linha_whitespace,coluna_whitespace,linha_whitespace+1,coluna_whitespace):
+               print("Movimento invalido")
+            
+         case "R":
+            if not self.tabuleiro.troca_posicoes(linha_whitespace,coluna_whitespace,linha_whitespace,coluna_whitespace+1):
+               print("Movimento invalido")
+         case "L":
+            if not self.tabuleiro.troca_posicoes(linha_whitespace,coluna_whitespace,linha_whitespace,coluna_whitespace-1):
+               print("Movimento invalido")
+             
+
+
+jogo1 = Jogo(3)
+
+jogo1.jogar()
---
+++ python_folder/joguinho/tabuleiro.py
@@ -0,0 +1,69 @@
+import random
+
+class Tabuleiro:
+
+   WHITE_SPACE = -1
+
+   tabuleiro: list[list[int]]
+   posicao_white_space: dict[str,int]
+   tamanho: int
+
+   def __init__(self, tamanho:int) -> None:
+      self.tamanho = tamanho
+      self.__criar_tabuleiro(tamanho)
+
+   def __criar_tabuleiro(self,tamanho:int)-> None:     
+      lista_num: list[int] = [i for i in range(tamanho*tamanho)] #cria a lista de numeros de 0 até n-1
+      random.shuffle(lista_num) #embaralha os items da lista
+      index_whitespace:int = lista_num.index(0) #acha o index de 0
+      lista_num[index_whitespace] = self.WHITE_SPACE #zero vai virar -1/whitespace
+      
+      #dividir a posicao no array do index do -1 pelo tamanho do array vai dar a linha do whitespace
+      #fazer o mod da posicao no array do index do -1 pelo tamanho do array vai dar a coluna do whitespace
+      self.posicao_white_space= [int(index_whitespace/self.tamanho) ,  index_whitespace%self.tamanho]
+     
+      self.tabuleiro = [ lista_num[(i*self.tamanho) : (i*self.tamanho) + self.tamanho]  for i in range(tamanho)   ] #lista comprehension num slice do vetor para gerar o tabuleiro
+
+   def print_tabuleiro(self)->None:
+      for i in range(self.tamanho):
+            print("+---" * self.tamanho + "+")
+            
+            for j in range(self.tamanho):
+               print("|", end="")
+               val = self.tabuleiro[i][j]
+               if val == self.WHITE_SPACE:
+                  print("   ", end="")
+               else:
+                  print(f"{val:3d}", end="")
+            
+            print("|")
+         
+      print("+---" * self.tamanho + "+")
+
+   def condicao_vitoria(self)->bool:
+      if self.tabuleiro[0][0] != self.WHITE_SPACE:
+         return False
+      
+      cur_value: int = 1
+      for i in range(self.tamanho):
+         for j in range(self.tamanho):
+             if i == 0 and j == 0:
+                continue
+             if self.tabuleiro[i][j] != cur_value:
+                return False
+             cur_value+=1
+         
+   def troca_posicoes(self, i1:int, j1:int, i2:int, j2:int)->bool:
+      if any( x < 0 for x  in[i1, j1, i2, j2])  or any( x >= self.tamanho for x in [i1, j1, i2, j2]):     
+          return False
+
+      tmp: int = self.tabuleiro[i1][j1]
+      self.tabuleiro[i1][j1] = self.tabuleiro[i2][j2] 
+      self.tabuleiro[i2][j2]  = tmp
+
+      if   [i1,j1] == self.posicao_white_space:
+           self.posicao_white_space = [i2,j2]
+      elif [i2,j2] == self.posicao_white_space:
+           self.posicao_white_space = [i1,j1]
+
+      return True
