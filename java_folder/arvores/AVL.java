b3f83f80946c1e40ede0a17de9c6ebfd220417f5
caue <cauepaivalira@outlook.com>
2024-04-21 14:50:52 -0300

    avl quase finalizada

---
+++ java_folder/arvores/AVL.java
@@ -0,0 +1,172 @@
+package java_folder.arvores;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class AVL extends ArvoreBin {
+
+   private static final int maxBalancingFactor = 1;
+
+
+
+   public static void main(String[] args) {
+      AVL avl = new AVL(50);
+
+      avl.Insert("ccc");
+      avl.Insert("ddd");
+      avl.Insert("eee");
+      avl.Insert("bbb");
+      avl.Insert("abaa");
+      avl.Insert("aaa");
+      
+      avl.__MakeRotation(1);
+      System.out.println(avl.toString());
+   }
+
+   AVL(int len) {
+      super(len);
+   }
+
+   private boolean __NodeIsBalanced(int index){
+      int balancingFactor = this.__GetBalancing(index);
+      
+      if (Math.abs(balancingFactor) > maxBalancingFactor)
+         return false;
+      else
+         return true;
+   }
+
+   private int __GetBalancing (int index)  {
+      if (index > this.lastNodeIndex) {
+         System.out.println("WARNING: Acessando a altura de um nó que não existe " + index + " retornando altura 0");
+         return 0;
+      }
+      //System.out.println("index entrando "+ index);
+      int leftHeight =   this.__GetHeightRecu(__LeftChild(index));
+
+      int rightHeight =  this.__GetHeightRecu(__RightChild(index));
+      //System.out.println("altura esq "+ leftHeight + " altura dir" + rightHeight);
+
+      return leftHeight - rightHeight;
+   }
+
+   private int __GetHeightRecu(int index) {
+         if (index > this.lastNodeIndex || this.nodeList[index] == null) 
+            return 0; 
+         
+         int leftHeight =  1 + this.__GetHeightRecu((index*2)+1);
+         int rightHeight = 1 + this.__GetHeightRecu((index*2)+2);
+
+         if (leftHeight > rightHeight)
+            return leftHeight;
+         else 
+            return rightHeight;
+   }
+
+   private void __MakeRotation(int index) {
+      if (index > this.lastNodeIndex) {
+         System.out.println("WARNING: tentando rotacionar um nó que não existe " + index);
+         return;
+      }
+
+      int rootBalancingFactor = this.__GetBalancing(index);
+      //System.out.println("fator balan" + rootBalancingFactor);
+
+      if (rootBalancingFactor > 0 ){ //valor positivo, ta pendendo pra esquerda
+         int leftChildBalancing =  this.__GetBalancing(__LeftChild(index));
+         
+         if (leftChildBalancing > 0){ //pai e filho tem o mesmo sinal no balanceamento, rotação DIR-DIR simples
+            this.__RotationLogic(index, true);
+         } else {
+             //rotação ESQ-DIR
+             this.__RotationLogic(__LeftChild(index), false); //rotação ESQ-ESQ no filho esquerdo do desbalanceado
+             this.__RotationLogic(index, true); //rotação DIR-DIR no nó desbalanceado
+         }
+
+      } else { //Valor negativo, ta pendendo pra direita
+         int rightChildBalancing = this.__GetBalancing(__RightChild(index));
+
+         if (rightChildBalancing < 0 ){ //pai e filho tem o mesmo sinal no balanceamento, rotação ESQ-ESQ simples
+            this.__RotationLogic(index, false);  
+         } else {
+            this.__RotationLogic(__RightChild(index), true); //rotação DIR_DIR no filho direito do desbalanceado
+            this.__RotationLogic(index, false); //rotação ESQ-ESQ no nó desbalanceado
+         }
+      }
+
+      this.__FindLastNodeIndex(); //acha o novo ultimo nó da arvore
+   }
+
+   private void __RotationLogic(int rootIndex, boolean rightRotation) {
+      List<String> UnbalancedSubTreeNodes = this.__GetSubtreeVals(rootIndex);
+      List<String> restOfTreeList = this.__CopyArrayExcluding(UnbalancedSubTreeNodes);
+
+      ArvoreBin balancedPart = new ArvoreBin(this.maxNodes);
+      ArvoreBin rotatePart = new ArvoreBin(this.maxNodes);
+      ArvoreBin finalBalancedTree = new ArvoreBin(this.maxNodes); //arvore com o vetor final
+
+      balancedPart.InsertList(restOfTreeList); //insera todos os nós da avl menos o da subarvore balanceada
+      
+      if (rightRotation)
+         rotatePart.Insert(UnbalancedSubTreeNodes.get(1)); //coloca o filho esquerdo da raiz como nova raiz (rotação DIR_DIR) da sub-arvore desbalanceada
+      else 
+         rotatePart.Insert(UnbalancedSubTreeNodes.get(2));//coloca o filho direito da raiz como nova raiz (rotação ESQ-ESQ) da sub-arvore desbalanceada
+      
+      rotatePart.Insert(UnbalancedSubTreeNodes.get(0)); //coloca o nó raiz da sub-arvore desbalanceada
+      rotatePart.InsertList(UnbalancedSubTreeNodes); //coloca o resto dos nós desbalanceados, os já adicionados não serão colocados dnv, ja que tem um check no método .Insert()
+
+      finalBalancedTree.InsertList(balancedPart.ListOfNodes());
+      finalBalancedTree.InsertList(rotatePart.ListOfNodes());
+
+      this.__CopyNodeList(finalBalancedTree.nodeList);
+   }
+
+   private List<String> __GetSubtreeVals(int rootIndex){
+      if (rootIndex > this.lastNodeIndex) {
+         return new ArrayList<>(); // Retorna uma lista vazia se o index da raiz for maior que o ultimo no
+      }
+   
+      List<String> values = new ArrayList<>(); //lista para guardar os valores da string em ordem
+      Queue<Integer> queue = new LinkedList<>();//fila para percorre o index de nós
+      
+      queue.offer(rootIndex); //adiciona o nó raiz na fila
+
+      while (!queue.isEmpty()){
+         
+         int currentIndex = queue.poll(); //tira o index atual da fila
+         values.add(this.nodeList[currentIndex]); //adiciona valor atual na lista de valores
+
+         int leftIndex = __LeftChild(currentIndex);
+         int rightIndex = __RightChild(currentIndex);
+
+         if (leftIndex <= lastNodeIndex && this.nodeList[leftIndex] != null) {
+             queue.offer(leftIndex);
+         }
+         if (rightIndex <= lastNodeIndex && this.nodeList[rightIndex] != null) {
+             queue.offer(rightIndex);
+         }
+      }
+
+      return values;
+   
+   }
+
+   //copia o array de nós da AVL mas excluindo certos valores de uma lista passada como parametro
+   private List<String> __CopyArrayExcluding(List<String> exclusionList){
+      List<String>  returnList = new ArrayList<>();
+         
+      for (int i = 0; i < this.maxNodes; i++) {
+            String curVal = this.nodeList[i];
+            if (curVal == null)
+               continue;
+            if (!exclusionList.contains(curVal)){ //valor não esta na lista de exclusão
+                returnList.add(curVal);  
+            }
+      }
+      return returnList;
+   }
+
+
+}
--- java_folder/arvores/ArvoreBin.java
+++ java_folder/arvores/ArvoreBin.java
@@ -1,15 +1,36 @@
 package java_folder.arvores;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArvoreBin {
 
-   private String[] nodeList;
+   protected String[] nodeList;
+   protected int lastNodeIndex; //guarda o index do ultimo no do array, nesse caso é um valor inclusivo a ser checado nos IFs, já que ele contem um valor válido
    public int maxNodes;
    public int nodeNumber;
+
+   protected static int __LeftChild (int index) {
+      return (index*2) +1;
+   }
+
+   protected static int __RightChild (int index) {
+      return (index*2) +2;
+   }
+
+   public static void main(String[] args) {
+      ArvoreBin arvo = new ArvoreBin(8);
+
+      arvo.Insert("abaa");
+      arvo.Insert("bbb");
+      arvo.Insert("aaa");
+        
+   }
 
    ArvoreBin(int len){
       this.nodeNumber = 0;
       this.maxNodes = len;
       this.nodeList = new String[len];
+      this.lastNodeIndex = -1;
    }
 
    public boolean Insert(final String value) {
@@ -17,103 +38,159 @@ public class ArvoreBin {
          return false;
       }
 
+      if(this.Find(value)){ //caso o valor ja exista, vamos retornar true ja que não tem nenhum erro mas não vamos inserir
+         return true;
+      }
+
       int indexToInsert = this.__InsertFindIndex(0, value);
+      //System.out.printf("index inserir %s é %d%n", value, indexToInsert);
 
       this.nodeList[indexToInsert] = value;
       this.nodeNumber++;
+      if (this.lastNodeIndex < indexToInsert) //atualiza o index do ultimo no do array
+         this.lastNodeIndex = indexToInsert;
       return true;
    }
 
-   private int __InsertFindIndex(final int curIndex, final String value) { //acha o index certo da string para ser inserida
-         if (curIndex >= this.maxNodes)
-           throw new IndexOutOfBoundsException("Index out of bounds: " + curIndex);
-         
-         String curNodeStr = this.nodeList[curIndex];
-         if (curNodeStr == null) //achou um lugar para inserir
-            return curIndex;
-         
-         if (value.compareTo(curNodeStr) < 0 ) { // string a ser inserida é menor que o pai, chama filho da esquerda
-            return this.__InsertFindIndex((curIndex*2)+1, value);
-         } else { // string a ser inserida é maior/igual que o pai, chama filho da direita
-            return this.__InsertFindIndex((curIndex*2)+2, value);
-         }
+   public boolean InsertList(final List<String> list) {        
+      
+      for (int i = 0; i < list.size(); i++) {
+            boolean insertResult = this.Insert(list.get(i)); 
+            if (!insertResult)
+               return false; //falha na inserção   
+      }
+      return true;
    }
 
-   public boolean Remove(final int index) {
-         String value = this.nodeList[index];
-         if (value == null)   //index não tem nenhum elemento
-            return false;
-         
-         this.nodeList[index] = null; 
-         this.nodeNumber--;
-         return true;
+   public boolean Remove(final String value) {
+      for (int i = 0; i <= this.lastNodeIndex; i++) {
+         if (this.nodeList[i] == value) {
+               this.nodeList[i] = null;
+               this.__RemoveChild(__LeftChild(i));
+               this.__RemoveChild(__RightChild(i));
+               break;
+         }
+      }
+      this.__FindLastNodeIndex();
+      this.nodeNumber--;
+      return true;
    }
 
    public int Len() {
       return this.nodeNumber;
    }
 
-   public boolean Find(final String valorBuscado) {      
-      return this.__FindRecursive(0, valorBuscado);
+   public boolean Find(final String value) {      
+      return this.__FindRecursive(0, value);
    }
 
-   private boolean __FindRecursive(final int curIndex, final String value){
-         if (curIndex >= this.maxNodes)
-           return false;
-         
-         String curNodeStr = this.nodeList[curIndex];
-         if (curNodeStr == null) //achou um lugar para inserir
-            return false;
-         
-         if (curNodeStr == value)
-            return true;
-         
-         if (value.compareTo(curNodeStr) < 0 ) { // string a ser inserida é menor que o pai, chama filho da esquerda
-            return this.__FindRecursive((curIndex*2)+1, value);
-         } else { // string a ser inserida é maior/igual que o pai, chama filho da direita
-            return this.__FindRecursive((curIndex*2)+2, value);
-         }
+   //retorna uma lista de nós em ordem baseado no array da AB, não tem a indexação direta de pais e filhos
+   public List<String> ListOfNodes(){
+      List<String> returnList = new ArrayList<>();
+      for (int i = 0; i < this.maxNodes; i++) {
+            String curVal = this.nodeList[i];
+            if (curVal == null)
+               continue;
+            returnList.add(curVal);
+      }
+      return returnList;
    }
 
    public void PrintTree() {
-      for (int i = 0; i < this.nodeNumber; i++) {
+      if (this.nodeNumber == 0){
+         System.out.println("arvore vazia");
+         return;
+      }
+      for (int i = 0; i <= this.lastNodeIndex; i++) {
             System.out.printf("%s%n", this.nodeList[i]);
       }
    }
 
    public String toString() {
       String treeStr = "";
-      boolean hasChild = false;
 
-      for (int i = 0; i < this.nodeNumber; i++) {
-            int leftChildIndex = (i*2)+1;
-            int rightChildIndex = (i*2)+2;
+      for (int i = 0; i <= this.lastNodeIndex; i++) {
+            int leftChildIndex = __LeftChild(i);
+            int rightChildIndex = __RightChild(i);
             String curStr = this.nodeList[i];
-            hasChild = false;
+            if (curStr == null)
+               continue;
+        
 
-            if (leftChildIndex < this.nodeNumber){ //caso o index seja menor que o número de nos na arvore, então existe um filho
+            if (leftChildIndex <= this.lastNodeIndex && this.nodeList[leftChildIndex] != null){ //caso o index seja menor ou igual ao ultimo no na arvore, então existe um filho
                String leftChildStr = this.nodeList[leftChildIndex];
                String formattedString = String.format( "%d %s ->  %d %s", i , curStr, leftChildIndex, leftChildStr);
                treeStr += (formattedString + "\n"); //soma string formatada com conexão entre pai e filho no resultado
-               hasChild = true;
+              
             }
-            if (rightChildIndex < this.nodeNumber){
+            if (rightChildIndex <= this.lastNodeIndex && this.nodeList[rightChildIndex] != null){
                String rightChildStr = this.nodeList[rightChildIndex];
                String formattedString = String.format( "%d %s ->  %d %s", i , curStr, rightChildIndex, rightChildStr);
                treeStr += (formattedString + "\n");
-               hasChild = true;
             }
 
-            if (!hasChild){ //caso o no não tenha filhos, vamos printar ela apenas
-               String formattedString = String.format( "%d %s -> null", i , curStr);
-               treeStr += (formattedString + "\n");
-            }
       }
       return treeStr;
    }
 
+   //MÉTODOS PRIVADOS
 
-   //Métodos protegidos para que as subclasses consigam manipular a heap, mas o usuaŕio final não  
+   private int __InsertFindIndex(final int curIndex, final String value) { //acha o index certo da string para ser inserida
+      if (curIndex >= this.maxNodes)
+        throw new IndexOutOfBoundsException("Index fora do tam maximo do array: " + curIndex);
+      
+      String curNodeStr = this.nodeList[curIndex];
+      if (curNodeStr == null) //achou um lugar para inserir
+         return curIndex;
+      
+      if (value.compareTo(curNodeStr) < 0 ) { // string a ser inserida é menor que o pai, chama filho da esquerda
+         return this.__InsertFindIndex(__LeftChild(curIndex), value);
+      } else { // string a ser inserida é maior/igual que o pai, chama filho da direita
+         return this.__InsertFindIndex(__RightChild(curIndex), value);
+      }
+   }
+
+   private void __RemoveChild(final int indexToRemove) {
+      if (indexToRemove > this.lastNodeIndex)
+         return;
+
+      this.nodeList[indexToRemove] = null;
+      this.nodeNumber--;
+      this.__RemoveChild(__LeftChild(indexToRemove));
+      this.__RemoveChild(__RightChild(indexToRemove));
+   }
+
+   private boolean __FindRecursive(final int curIndex, final String value){
+      if (curIndex >= this.maxNodes)
+        return false;
+      
+      String curNodeStr = this.nodeList[curIndex];
+      if (curNodeStr == null) //não achou
+         return false;
+      
+      if (curNodeStr.equals(value))
+         return true;
+      
+      if (value.compareTo(curNodeStr) < 0 ) { // string a ser inserida é menor que o pai, chama filho da esquerda
+         return this.__FindRecursive(__LeftChild(curIndex), value);
+      } else { // string a ser inserida é maior/igual que o pai, chama filho da direita
+         return this.__FindRecursive(__RightChild(curIndex), value);
+      }
+}
+
+   //apos certas operações é necessário achar de novo qual o ultimo no da árvore
+   protected void __FindLastNodeIndex() {
+         for (int i = this.maxNodes-1; i >= 0; i--) { 
+             if (this.nodeList[i] != null){
+                  this.lastNodeIndex = i;
+                  return;
+             }
+         } 
+         this.lastNodeIndex = -1; //não achamos nenhum valor diferente de null na lista, então ela esta vazia
+   }
+
+   //MÉTODOS PROTEGIDOS  
+   //Usados para que as subclasses consigam manipular a heap, mas o usuaŕio final não  
 
    protected String _GetNode(final int i) {
      if (i >= this.maxNodes) 
@@ -130,34 +207,45 @@ public class ArvoreBin {
 
    //dado um nó raiz de sub árvore i, conta quantos nós essa sub aŕvore tem (contando com a raiz)
    protected int _CountNodes(final int i){
-      if (i >= this.nodeNumber)
+      if (i > this.lastNodeIndex)
          return 0; //caso base da recursão 
 
-      return 1 + this._CountNodes((i*2)+1) + this._CountNodes((i*2)+2);
+      return 1 + this._CountNodes(__LeftChild(i)) + this._CountNodes(__RightChild(i));
 
    }
 
    protected int _NodeRight (final int i){
-      if (i >= this.nodeNumber)
+      if (i > this.lastNodeIndex)
          return -1;
 
-      int rightChildIndex = (i*2)+2;
-      if (rightChildIndex >= this.nodeNumber)
+      int rightChildIndex = __RightChild(i);
+      if (rightChildIndex > this.lastNodeIndex)
           return -1;
       else
          return rightChildIndex;
    }
 
    protected int _NodeLeft (final int i){
-      if (i >= this.nodeNumber)
+      if (i > this.lastNodeIndex)
          return -1;
 
-      int leftChildIndex = (i*2)+1;
-      if (leftChildIndex >= this.nodeNumber)
+      int leftChildIndex = __LeftChild(i);
+      if (leftChildIndex > this.lastNodeIndex)
           return -1;
       else
          return leftChildIndex;
    }
 
+   protected void __CopyNodeList(String[] newVals){
+      if (newVals.length != this.nodeList.length){
+         System.out.println("WARNING: arrays são de tamanhos diferentes, operação falhou");
+         return;
+      }
+
+      for (int i = 0; i < newVals.length; i++) {
+         this.nodeList[i] = newVals[i]; 
+      }
+
+   }
 
 }
